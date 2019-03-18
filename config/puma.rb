805dbd4920adf66f64b49826599cbac4c93ffe04
Nick Silverman <nckslvrmn@gmail.com>
2019-03-18 14:04:58 -0400

    bind on sockets

--- config/puma.rb
+++ config/puma.rb
@@ -2,4 +2,11 @@ environment 'production'
 pidfile './puma.pid'
 workers 2
 worker_timeout 60
-bind "tcp://#{ENV['BIND_ADDRESS']}:#{ENV['BIND_PORT']}"
+if ENV['BIND_ADDRESS'] && ENV['BIND_PORT']
+  bind "tcp://#{ENV['BIND_ADDRESS']}:#{ENV['BIND_PORT']}"
+elsif ENV['BIND_SOCK']
+  bind "unix://#{ENV['BIND_SOCK']}"
+else
+  puts "expecting BIND_ADDRESS/BIND_PORT or BIND_SOCK environment variables to bind"
+  exit 1
+end
