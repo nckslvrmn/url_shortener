3fe781a02bf2630efcd6ce7ce2e551d53f4c1559
Nick Silverman <nckslvrmn@gmail.com>
2020-12-18 12:24:30 -0500

    shorten url id and retry on non-uniqueness and lint function

--- lambda.py
+++ lambda.py
@@ -1,54 +1,62 @@
+import base64
 import boto3
 import json
 import random
-import re
 import string
 import time
 
+
 def store(env):
-    if env['body']['full_url'] == None or env['body']['full_url'] == '':
+    if env['body']['full_url'] is None or env['body']['full_url'] == '':
         return {}
-    short_url_id = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(16))
-    dynamo = boto3.client('dynamodb', region_name = 'us-east-1')
-    response = dynamo.put_item(
-        TableName = 'URLDB',
-        Item = {
-            'short_url_id': { 'S': short_url_id },
-            'full_url': { 'S': env['body']['full_url'] },
-            'created_at': { 'N': str(int(time.time())) },
-            'ttl': { 'N': str(int(time.time()) + (86400 * 30)) }
-        }
-    )
-    return {
-        'statusCode': 200,
-        'headers': { 'Content-Type': 'application/json' },
-        'body': json.dumps({ 'short_url_id': short_url_id })
-    }
+    dynamo = boto3.client('dynamodb', region_name='us-east-1')
+    while True:
+        try:
+            short_url_id = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
+            dynamo.put_item(
+                TableName='URLDB',
+                Item={
+                    'short_url_id': {'S': short_url_id},
+                    'full_url': {'S': env['body']['full_url']},
+                    'created_at': {'N': str(int(time.time()))},
+                    'ttl': {'N': str(int(time.time()) + (86400 * 30))}
+                },
+                ConditionExpression='attribute_not_exists(short_url_id)'
+            )
+            return {
+                'statusCode': 200,
+                'headers': {'Content-Type': 'application/json'},
+                'body': json.dumps({'short_url_id': short_url_id})
+            }
+        except dynamo.exceptions.ConditionalCheckFailedException:
+            pass
+
 
 def redirect(env):
     short_url_id = env['path'].split('/')[-1]
-    dynamo = boto3.client('dynamodb', region_name = 'us-east-1')
+    dynamo = boto3.client('dynamodb', region_name='us-east-1')
     response = dynamo.get_item(
-        TableName = 'URLDB',
-        Key = { 'short_url_id': { 'S': short_url_id } },
-        AttributesToGet = ['full_url'],
+        TableName='URLDB',
+        Key={'short_url_id': {'S': short_url_id}},
+        AttributesToGet=['full_url'],
     )
-    if response['Item']['full_url']['S'] == None:
-        return { 'statusCode': 404, 'headers': {}, 'body': '' }
+    if response['Item']['full_url']['S'] is None:
+        return {'statusCode': 404, 'headers': {}, 'body': ''}
     else:
-        return { 'statusCode': 302, 'headers': { 'Location': response['Item']['full_url']['S'] }, 'body': '' }
+        return {'statusCode': 302, 'headers': {'Location': response['Item']['full_url']['S']}, 'body': ''}
+
 
 def router(env):
-    p = re.compile(r'/r/+')
     if env['path'] == '/store' and env['method'] == 'POST':
         return store(env)
-    elif p.match(env['path']) and env['method'] == 'GET':
+    elif env['method'] == 'GET':
         return redirect(env)
     else:
-        return { 'statusCode': 404, 'headers': {}, 'body': '' }
+        return {'statusCode': 404, 'headers': {}, 'body': ''}
+
 
 def handler(event, context):
-    if event['body'] == None:
+    if event['body'] is None:
         body = None
     elif event['isBase64Encoded']:
         body = json.loads(base64.b64decode(event['body']))
@@ -58,7 +66,7 @@ def handler(event, context):
     env = {
         'method': event['requestContext']['httpMethod'],
         'path': event['requestContext']['path'],
-        'params': event['queryStringParameters'] if event['queryStringParameters'] != None else '',
+        'params': event['queryStringParameters'] if event['queryStringParameters'] is not None else '',
         'body': body
     }
 
